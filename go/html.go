c9c9be0b2065cc237477760677326f8e61f2dbd3
skuzzymiglet <skuzzymiglet@gmail.com>
2021-01-29 15:25:54 +0000

    parser v2 - hack to avoid italicizing URLs

--- go/html.go
+++ go/html.go
@@ -8,7 +8,8 @@ import (
 
 func link(s string) string {
 	parts := strings.SplitN(s[2:len(s)-2], ":", 2)
-	return fmt.Sprintf("<a href=\"%s\">%s</a>", parts[1], parts[0])
+	return fmt.Sprintf("<a href=\"%s\">%s</a>", strings.TrimSpace(parts[1]), strings.TrimSpace(parts[0]))
+	// TODO: allow HTML in link text
 }
 
 var funcs = map[string]func(string) string{
@@ -16,15 +17,15 @@ var funcs = map[string]func(string) string{
 }
 
 var replaces = map[string]string{
-	`\/\/(.*)\/\/`: "<i>$1</i>",
-	`_(.*)_`:       "<i>$1</i>",
-	`==(.*)==`:     "<b>$1</b>",
-	`\*(.*)\*`:     "<b>$1</b>",
-	`'(.*)'`:       "<code>$1</code>",
-	"`(.*)`":       "<code>$1</code>",
+	` \/\/(.*)\/\/ `: " <i>$1</i> ",
+	`_(.*)_`:         "<i>$1</i>",
+	`==(.*)==`:       "<b>$1</b>",
+	`\*(.*)\*`:       "<b>$1</b>",
+	`'(.*)'`:         "<code>$1</code>",
+	"`(.*)`":         "<code>$1</code>",
 }
 
-func stringRenderHTML(in string) string {
+func renderSimpleHTML(in string) string {
 	var (
 		buf = in
 	)
@@ -39,4 +40,39 @@ func stringRenderHTML(in string) string {
 		buf = re.ReplaceAllString(buf, v)
 	}
 	return buf
+}
+
+func renderTitleHTML(in string) string {
+	// TODO: not require a space here
+	// jesus christ this is opaque
+	var level int
+	for i, c := range in {
+		if c != '#' {
+			level = i
+			break
+		}
+	}
+
+	return fmt.Sprintf("<h%d>%s</h%d>", level, in[level:], level)
+}
+
+func renderLineHTML(in string) string {
+	if len(in) < 1 {
+		return ""
+	}
+
+	switch in[0] {
+	case '#':
+		return renderTitleHTML(in)
+	}
+	return renderSimpleHTML(in)
+}
+
+func stringRenderHTML(in string) string {
+	// TODO: correctly handle paragraphs
+	var buf strings.Builder
+	for _, l := range strings.Split(in, "\n") {
+		buf.WriteString(renderLineHTML(l) + "\n")
+	}
+	return buf.String()
 }
--- go/main.go
+++ go/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/kr/pretty"
 
 func main() {
-	pretty.Println(stringRenderHTML("==lol gay== //fuck// *shit* _cock_ 'piss' `ass` and foof lol [[link: https://skuz.xyz]]"))
+	pretty.Println(stringRenderHTML("==lol gay== //fuck// *shit* _cock_ 'piss' `ass` and foof lol [[link: https://skuz.xyz/go-in-go.html]]\n\nlol ==l00l i said==\n## fuck"))
 }
